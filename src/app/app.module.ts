24a1321a7de8a7405a44fe740bdc0f659307b2bb
Mohamed EL-Ghoneimy <ghoneishy@hotmail.com>
2018-12-03 23:59:05 +0200

    adding ngx-Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './ui/layout/layout.component';
 import { HeaderComponent } from './ui/header/header.component';
 import { FooterComponent } from './ui/footer/footer.component';
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +13,7 @@ import { FooterComponent } from './ui/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    NgbModule.forRoot(),
+    TabsModule.forRoot(),
     BrowserModule
   ],
   exports: [LayoutComponent],
