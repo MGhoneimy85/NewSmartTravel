2e18b487011a4cb026bc123f9b51e0afcd85bd7f
Mohamed EL-Ghoneimy <ghoneishy@hotmail.com>
2018-12-03 23:29:21 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './ui/layout/layout.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from './ui/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule
   ],
+  exports: [LayoutComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
