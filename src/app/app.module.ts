b2035ed43187f162d99b4e1267624988385d9912
Mohamed EL-Ghoneimy <ghoneishy@hotmail.com>
2018-12-05 23:43:56 +0200

    adding arabic translation sticky menu and smoth scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './ui/layout/layout.component';
-import { HeaderComponent } from './ui/header/header.component';
-import { FooterComponent } from './ui/footer/footer.component';
-import { TabsModule } from 'ngx-bootstrap/tabs';
+import { LayoutComponent } from './master/layout/layout.component';
+import { HeaderComponent } from './master/header/header.component';
+import { FooterComponent } from './master/footer/footer.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/translation/', '.json');
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +19,14 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
     FooterComponent
   ],
   imports: [
-    TabsModule.forRoot(),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     BrowserModule
   ],
   exports: [LayoutComponent],
---
+++ src/app/master/header/header.component.ts
@@ -0,0 +1,45 @@
+import { Component, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements AfterViewInit {
+  sticky: Boolean = false;
+  menuPosition: any;
+  menuItems = ['home' , 'packages' , 'ourcompany' , 'partners' , 'contactus'];
+
+  clicked = 'home';
+  @ViewChild('stickyMenu') menuElement: ElementRef;
+
+  constructor(private language: TranslateService) { }
+
+
+  ngAfterViewInit() {
+    this.menuPosition = this.menuElement.nativeElement.offsetTop;
+    console.log(this.menuPosition);
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  handleScroll() {
+    const windowScroll = window.pageYOffset;
+    if (windowScroll >= this.menuPosition) {
+      this.sticky = true;
+    } else {
+      this.sticky = false;
+    }
+  }
+  scroll(id, event) {
+    const el = document.getElementById(id);
+    event.preventDefault();
+    event.target.className = 'nav-item nav-link active';
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  onLangChange(lang: string) {
+    this.language.use(lang);
+  }
+
+}
