6d70b5da2c70c35d885ea8460047d75e5c604563
Mohamed EL-Ghoneimy <ghoneishy@hotmail.com>
2018-12-07 16:38:41 +0200

    contact us Done // TODO contact and arabic translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PackagesComponent } from './packages/packages.component';
 import { OurCompanyComponent } from './our-company/our-company.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { OwlModule } from 'ngx-owl-carousel';
+
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/translation/', '.json');
--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ViewChild,  OnInit } from '@angular/core';
+// import {  } from 'googlemaps';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
-
+  @ViewChild('gmap') gmapElement: any;
   constructor() { }
-
+  map: google.maps.Map;
   ngOnInit() {
+    const mapProp = {
+      center: new google.maps.LatLng(30.048181, 31.201487),
+      zoom: 18,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
 }
